0ea9d894da52313a157636ed45208156bc050cf9
Stiveys <stevomwaura@gmail.com>
2025-02-17 15:05:00 +0300

    new

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
+# lib/student.py
 
-from user import User
+from user import User  # Ensure you import the User class from user.py
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, knowledge_string):
+        self.knowledge.append(knowledge_string)
